417220df7b5f442a9c8529171a618bcfa905650e
nipdep <nipun1deelaka@gmail.com>
2022-03-28 20:47:03 +0530

    update AST part2

--- src/ast/dataloaderV2.py
+++ src/ast/dataloaderV2.py
@@ -169,40 +169,11 @@ class AudiosetDataset(Dataset):
         noise, _ = get_noise_sample()
         self.noises = noise
 
-    def _wav2fbank(self, filename, filename2=None):
-        # mixup
-        if filename2 == None:
-            # waveform, sr = torchaudio.load(filename)
-            waveform, sr = get_sample(filename, self.noises)
-            waveform = waveform.type(torch.float32)
-            waveform = waveform - waveform.mean()
-        # mixup
-        else:
-            waveform1, sr = torchaudio.load(filename)
-            waveform2, _ = torchaudio.load(filename2)
+    def _wav2fbank(self, filename):
 
-            waveform1 = waveform1 - waveform1.mean()
-            waveform2 = waveform2 - waveform2.mean()
-
-            if waveform1.shape[1] != waveform2.shape[1]:
-                if waveform1.shape[1] > waveform2.shape[1]:
-                    # padding
-                    temp_wav = torch.zeros(1, waveform1.shape[1])
-                    temp_wav[0, 0:waveform2.shape[1]] = waveform2
-                    waveform2 = temp_wav
-                else:
-                    # cutting
-                    waveform2 = waveform2[0, 0:waveform1.shape[1]]
-
-            # sample lambda from uniform distribution
-            #mix_lambda = random.random()
-            # sample lambda from beta distribtion
-            mix_lambda = np.random.beta(10, 10)
-
-            mix_waveform = mix_lambda * waveform1 + \
-                (1 - mix_lambda) * waveform2
-            waveform = mix_waveform - mix_waveform.mean()
-
+        waveform, sr = get_sample(filename, self.noises)
+        waveform = waveform.type(torch.float32)
+        waveform = waveform - waveform.mean()
 
         fbank = torchaudio.compliance.kaldi.fbank(waveform, htk_compat=True, sample_frequency=sr, use_energy=False,
                                                   window_type='hanning', num_mel_bins=self.melbins, dither=0.0, frame_shift=10)
@@ -219,10 +190,8 @@ class AudiosetDataset(Dataset):
         elif p < 0:
             fbank = fbank[0:target_length, :]
 
-        if filename2 == None:
-            return fbank, 0
-        else:
-            return fbank, mix_lambda
+        return fbank, 0
+
 
     def __getitem__(self, index):
         """
@@ -231,62 +200,37 @@ class AudiosetDataset(Dataset):
         audio is a FloatTensor of size (N_freq, N_frames) for spectrogram, or (N_frames) for waveform
         nframes is an integer
         """
-        # do mix-up for this sample (controlled by the given mixup rate)
-        if random.random() < self.mixup:
-            datum = self.data[index]
-            # find another sample to mix, also do balance sampling
-            # sample the other sample from the multinomial distribution, will make the performance worse
-            # mix_sample_idx = np.random.choice(len(self.data), p=self.sample_weight_file)
-            # sample the other sample from the uniform distribution
-            mix_sample_idx = random.randint(0, len(self.data)-1)
-            mix_datum = self.data[mix_sample_idx]
-            # get the mixed fbank
-            fbank, mix_lambda = self._wav2fbank(datum['wav'], mix_datum['wav'])
-            # initialize the label
-            label_indices = np.zeros(self.label_num)
-            # add sample 1 labels
-            # for label_str in datum['labels'].split(','):
-            #     label_indices[int(self.index_dict[label_str])] += mix_lambda
-            label_indices[datum['labels']] += mix_datum
-            # add sample 2 labels
-            # for label_str in mix_datum['labels'].split(','):
-            #     label_indices[int(self.index_dict[label_str])] += 1.0-mix_lambda
-            label_indices[mix_datum['labels']] += (1.0 - mix_lambda)
-            label_indices = torch.FloatTensor(label_indices)
-        # if not do mixup
-        else:
-            datum = self.data[index]
-            label_indices = np.zeros(self.label_num)
-            fbank, mix_lambda = self._wav2fbank(datum['wav'])
-            # for label_str in datum['labels'].split(','):
-            #     label_indices[int(self.index_dict[label_str])] = 1.0
-            label_indices[datum['labels']] += 1.0
 
-            label_indices = torch.FloatTensor([datum['labels']])
+        datum = self.data[index]
+        label_indices = np.zeros(self.label_num)
+        fbank, mix_lambda = self._wav2fbank(datum['wav'])
+        # for label_str in datum['labels'].split(','):
+        #     label_indices[int(self.index_dict[label_str])] = 1.0
+        label_indices[datum['labels']] += 1.0
+
+        label_indices = torch.FloatTensor([datum['labels']])
 
         # SpecAug, not do for eval set
-        freqm = torchaudio.transforms.FrequencyMasking(self.freqm)
-        timem = torchaudio.transforms.TimeMasking(self.timem)
-        fbank = torch.transpose(fbank, 0, 1)
-        if self.freqm != 0:
-            fbank = freqm(fbank)
-        if self.timem != 0:
-            fbank = timem(fbank)
-        fbank = torch.transpose(fbank, 0, 1)
+        # freqm = torchaudio.transforms.FrequencyMasking(self.freqm)
+        # timem = torchaudio.transforms.TimeMasking(self.timem)
+        # fbank = torch.transpose(fbank, 0, 1)
+        # if self.freqm != 0:
+        #     fbank = freqm(fbank)
+        # if self.timem != 0:
+        #     fbank = timem(fbank)
+        # fbank = torch.transpose(fbank, 0, 1)
 
         # normalize the input for both training and test
-        if not self.skip_norm:
-            fbank = (fbank - self.norm_mean) / (self.norm_std * 2)
-        # skip normalization the input if you are trying to get the normalization stats.
-        else:
-            pass
+        # if not self.skip_norm:
+        #     fbank = (fbank - self.norm_mean) / (self.norm_std * 2)
+        # # skip normalization the input if you are trying to get the normalization stats.
+        # else:
+        #     pass
 
         # if self.noise == True:
         #     fbank = fbank + torch.rand(fbank.shape[0], fbank.shape[1]) * (self.noise_lvl**0.5)  # Add gaussian noise with configured noise level
         #     fbank = torch.roll(fbank, np.random.randint(-10, 10), 0)
 
-        mix_ratio = min(mix_lambda, 1-mix_lambda) / \
-            max(mix_lambda, 1-mix_lambda)
 
         # the output fbank shape is [time_frame_num, frequency_bins], e.g., [1024, 128]
         return fbank, label_indices
@@ -340,36 +284,9 @@ class AudioTestDataset(Dataset):
         print('number of classes is {:d}'.format(self.label_num))
 
     def _wav2fbank(self, filename, filename2=None):
-        # mixup
-        if filename2 == None:
-            waveform, sr = torchaudio.load(filename)
-            waveform = waveform - waveform.mean()
-        # mixup
-        else:
-            waveform1, sr = torchaudio.load(filename)
-            waveform2, _ = torchaudio.load(filename2)
+        waveform, sr = torchaudio.load(filename)
+        waveform = waveform - waveform.mean()
 
-            waveform1 = waveform1 - waveform1.mean()
-            waveform2 = waveform2 - waveform2.mean()
-
-            if waveform1.shape[1] != waveform2.shape[1]:
-                if waveform1.shape[1] > waveform2.shape[1]:
-                    # padding
-                    temp_wav = torch.zeros(1, waveform1.shape[1])
-                    temp_wav[0, 0:waveform2.shape[1]] = waveform2
-                    waveform2 = temp_wav
-                else:
-                    # cutting
-                    waveform2 = waveform2[0, 0:waveform1.shape[1]]
-
-            # sample lambda from uniform distribution
-            #mix_lambda = random.random()
-            # sample lambda from beta distribtion
-            mix_lambda = np.random.beta(10, 10)
-
-            mix_waveform = mix_lambda * waveform1 + \
-                (1 - mix_lambda) * waveform2
-            waveform = mix_waveform - mix_waveform.mean()
 
         fbank = torchaudio.compliance.kaldi.fbank(waveform, htk_compat=True, sample_frequency=sr, use_energy=False,
                                                   window_type='hanning', num_mel_bins=self.melbins, dither=0.0, frame_shift=10)
@@ -386,10 +303,8 @@ class AudioTestDataset(Dataset):
         elif p < 0:
             fbank = fbank[0:target_length, :]
 
-        if filename2 == None:
-            return fbank, 0
-        else:
-            return fbank, mix_lambda
+        return fbank, 0
+
 
     def __getitem__(self, index):
         """
@@ -398,38 +313,11 @@ class AudioTestDataset(Dataset):
         audio is a FloatTensor of size (N_freq, N_frames) for spectrogram, or (N_frames) for waveform
         nframes is an integer
         """
-        # do mix-up for this sample (controlled by the given mixup rate)
-        if random.random() < self.mixup:
-            datum = self.data[index]
-            # find another sample to mix, also do balance sampling
-            # sample the other sample from the multinomial distribution, will make the performance worse
-            # mix_sample_idx = np.random.choice(len(self.data), p=self.sample_weight_file)
-            # sample the other sample from the uniform distribution
-            mix_sample_idx = random.randint(0, len(self.data)-1)
-            mix_datum = self.data[mix_sample_idx]
-            # get the mixed fbank
-            fbank, mix_lambda = self._wav2fbank(datum, mix_datum)
-            # initialize the label
-            # label_indices = np.zeros(self.label_num)
-            # add sample 1 labels
-            # for label_str in datum['labels'].split(','):
-            #     label_indices[int(self.index_dict[label_str])] += mix_lambda
-            # label_indices[datum['labels']] += mix_datum
-            # add sample 2 labels
-            # for label_str in mix_datum['labels'].split(','):
-            #     label_indices[int(self.index_dict[label_str])] += 1.0-mix_lambda
-            # label_indices[mix_datum['labels']] += (1.0 - mix_lambda)
-            # label_indices = torch.FloatTensor(label_indices)
-        # if not do mixup
-        else:
-            datum = self.data[index]
-            # label_indices = np.zeros(self.label_num)
-            fbank, mix_lambda = self._wav2fbank(datum)
-            # for label_str in datum['labels'].split(','):
-            #     label_indices[int(self.index_dict[label_str])] = 1.0
-            # label_indices[datum['labels']] += 1.0
 
-            # label_indices = torch.FloatTensor(label_indices)
+        datum = self.data[index]
+        # label_indices = np.zeros(self.label_num)
+        fbank, mix_lambda = self._wav2fbank(datum)
+
 
         # SpecAug, not do for eval set
         # freqm = torchaudio.transforms.FrequencyMasking(self.freqm)
@@ -442,18 +330,15 @@ class AudioTestDataset(Dataset):
         # fbank = torch.transpose(fbank, 0, 1)
 
         # normalize the input for both training and test
-        if not self.skip_norm:
-            fbank = (fbank - self.norm_mean) / (self.norm_std * 2)
-        # skip normalization the input if you are trying to get the normalization stats.
-        else:
-            pass
+        # if not self.skip_norm:
+        #     fbank = (fbank - self.norm_mean) / (self.norm_std * 2)
+        # # skip normalization the input if you are trying to get the normalization stats.
+        # else:
+        #     pass
 
         # if self.noise == True:
         #     fbank = fbank + torch.rand(fbank.shape[0], fbank.shape[1]) * (self.noise_lvl**0.5)  # Add gaussian noise with configured noise level
         #     fbank = torch.roll(fbank, np.random.randint(-10, 10), 0)
-
-        mix_ratio = min(mix_lambda, 1-mix_lambda) / \
-            max(mix_lambda, 1-mix_lambda)
 
         # the output fbank shape is [time_frame_num, frequency_bins], e.g., [1024, 128]
         return fbank #, label_indices
